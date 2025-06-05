ceb2f242f4867e9193558adaab9070b7a1cc7cd6
Andrew <Andry.sheremetew@gmail.com>
2025-06-06 01:01:55 +0300

    add lazy for dynamic layers (house components)

--- src/components/HouseComponent/HouseComponent.tsx
+++ src/components/HouseComponent/HouseComponent.tsx
@@ -11,18 +11,21 @@ export const HouseComponent = ({ layers, layerFilters }: HouseComponentProps) =>
       <img className={styles.base} src={base} alt="house background" />
       
       <img
+        loading="lazy"
         className={`${styles.wall} ${filterStyles[layerFilters.wall] || ""}`}
         src={layers.wall}
         alt="walls"
       />
 
       <img
+        loading="lazy"
         className={`${styles.angles} ${filterStyles[layerFilters.angles] || ""}`}
         src={layers.angles}
         alt="angles"
       />
 
       <img
+        loading="lazy"
         className={`${styles.corner} ${filterStyles[layerFilters.corner] || ""}`}
         src={layers.corner}
         alt="corner"
